bb2977324b3ac448060704bc22e0809f516c4815
felix <xLifeHD@gmail.com>
2016-02-08 14:03:50 +0100

    Configuration

--- InfiniteWorld_Plugin/src/com/gmail/xlifehd/infiniteworld/MovementListener.java
+++ InfiniteWorld_Plugin/src/com/gmail/xlifehd/infiniteworld/MovementListener.java
@@ -4,41 +4,42 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import com.gmail.xlifehd.infiniteworld.main;;
 
 public class MovementListener implements Listener {
 	
-	 @EventHandler
-	 public void onPlayerMove(PlayerMoveEvent event) {
-		 
-		 Location loc = event.getTo();
-		 int boundary = 100;
-		 int offsetx = 0;
-		 int offsetz = 0;
-		 int safezone = 5;
-		 int x = (int) loc.getX() - offsetx;
-		 int z = (int) loc.getZ() - offsetz;
-		 
-		 /*Debug
-		 if ( Math.abs(x) >= boundary || Math.abs(z) >= boundary ) {
-			 Bukkit.broadcastMessage("X: " + x + " | Z: " + z);
-		 }
+	@EventHandler
+	public void onPlayerMove (PlayerMoveEvent event) {
+		
+		main Main = new main();
+		int boundary = Main.config.getInt("Boundary");
+		int offsetx = Main.config.getInt("X-Offset");
+		int offsetz = Main.config.getInt("Z-Offset");
+		int bufferzone = Main.config.getInt("Bufferzone");
+		
+		Location loc = event.getTo();
+		int x = (int) loc.getX() - offsetx;
+		int z = (int) loc.getZ() - offsetz;
+		
+		/*Debug
+		 * 		 if ( Math.abs(x) >= boundary || Math.abs(z) >= boundary ) {
+			Bukkit.broadcastMessage("X: " + x + " | Z: " + z);
+		}
 		 */
-		 
-		 //X-Coordinate Handler
-		 if ( x >= boundary ) { 
-			 loc.setX( -x + offsetx + safezone );
-		 } else if ( x <= -boundary ) { 
-			 loc.setX( -x + offsetx - safezone ); 
-		 }
-		 
-		 //Y-Coordinate Handler
-		 if ( z >= boundary ) { 
-			 loc.setZ( -z + offsetz + safezone ); 
-		 } else if ( z <= -boundary ) { 
-			 loc.setZ( -z + offsetz - safezone ); 
-		 }
-		 
-
-	 }
+		
+		//X-Coordinate Handler
+		if ( x >= boundary ) { 
+			loc.setX( -x + offsetx + bufferzone );
+		} else if ( x <= -boundary ) { 
+			loc.setX( -x + offsetx - bufferzone ); 
+		}
+		
+		//Y-Coordinate Handler
+		if ( z >= boundary ) { 
+			loc.setZ( -z + offsetz + bufferzone ); 
+		} else if ( z <= -boundary ) { 
+			loc.setZ( -z + offsetz - bufferzone ); 
+		}
+	}
 	 
 }
--- InfiniteWorld_Plugin/src/com/gmail/xlifehd/infiniteworld/main.java
+++ InfiniteWorld_Plugin/src/com/gmail/xlifehd/infiniteworld/main.java
@@ -1,12 +1,25 @@
 package com.gmail.xlifehd.infiniteworld;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
 	
+	public FileConfiguration config = getConfig();
+	
 	@Override
 	public void onEnable() {
+		
+		//Config
+		config.addDefault("Boundary", 100);
+		config.addDefault("X-Offset", 0);
+		config.addDefault("Z-Offset", 0);
+		config.addDefault("Bufferzone", 5);
+		config.options().copyDefaults(true);
+		saveConfig();
+		
 		getServer().getPluginManager().registerEvents(new MovementListener(), this);
+		
 	}
 	
 	@Override
